a95515864ec2eef6465b534951c95a61c2833d7b
Williams E. Fernández <williamsenrique@gmail.com>
2023-09-02 12:48:55 -0600

    Agregando método con path variable y estableciendo configuración de log en debug

--- src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.lordwillfer.rest.webservices.restfulwebservices.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.lordwillfer.rest.webservices.restfulwebservices.bean.HelloWorldBean;
 
@@ -22,5 +23,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello Wolrd");
+	}
+
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello Wolrd, %s", name));
 	}
 }
