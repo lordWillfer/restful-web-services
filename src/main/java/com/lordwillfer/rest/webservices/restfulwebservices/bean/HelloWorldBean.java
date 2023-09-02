28a00e4d93e146e79302afbed4134782ff671608
Williams E. Fernández <williamsenrique@gmail.com>
2023-09-02 12:22:14 -0600

    Agregando un nuevo método al API para mostrar el mensaje desde un bean

---
+++ src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/bean/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.lordwillfer.rest.webservices.restfulwebservices.bean;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.lordwillfer.rest.webservices.restfulwebservices.bean.HelloWorldBean;
 
 // Controller
 @RestController
@@ -16,5 +17,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello Wolrd");
 	}
 }
