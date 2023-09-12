3405d1b8d085fbd73e8b38a93276012d5d34e524
Williams E. Fernández <williamsenrique@gmail.com>
2023-09-12 17:50:46 -0600

    Realizando ajuste para permitir envío de ";" en el Path

--- src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/lordwillfer/rest/webservices/restfulwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,10 +1,14 @@
 package com.lordwillfer.rest.webservices.restfulwebservices.basic.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +22,18 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
                 .anyRequest().authenticated().and()
                 //.and().formLogin().and()
                 .httpBasic();
+    }
+
+    @Bean
+    public HttpFirewall allowSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.httpFirewall(allowSemicolonHttpFirewall());
     }
 }
